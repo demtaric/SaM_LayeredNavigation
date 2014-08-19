88489cdec98518515b7ee504dc952c4d039a5240
Taras Demianchenko <demtarico@gmail.com>
2014-08-19 11:13:13 +0300

    Initial commit

---
+++ app/code/community/SaM/LayeredNavigation/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taric
+ * Date: 8/19/14
+ * Time: 11:09 AM
+ */ 
+class SaM_LayeredNavigation_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/community/SaM/LayeredNavigation/sql/sam_layerednavigation_setup/install-0.1.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taric
+ * Date: 8/19/14
+ * Time: 11:09 AM
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
